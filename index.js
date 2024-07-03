bb1ee9822f716dd875a5dee7459de2e1b3bab479
Al Haseeb Zafar <haseebzafar.07@gmail.com>
2024-07-03 14:20:16 +0500

    index.js

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express = require("express");
 require("./db/mongoose");
-
-// const userRouter = require("./routers/user");
-// const categoriesRouter = require("./router/categories");
-// const quotesRouter = require("./router/quotes");
+const userRouter = require("./routers/user");
+const quotesRouter = require("./router/quotes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-// app.use(userRouter);
-// app.use(categoriesRouter);
-// app.use(quotesRouter);
+app.use(userRouter);
+app.use(quotesRouter);
 
 app.listen(port, () => {
   console.log("server is up on port" + port);
 });
-
-const User = require("./models/users");
-const Category = require("./models/categories");
-const Quotes = require("./models/quotes");
